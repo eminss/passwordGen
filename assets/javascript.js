21f7343fe65d96caa831c966ebb348ac437ffe5a
Jaden Storey <jadenstorey@hotmail.com>
2022-03-31 23:23:33 -0700

    upload passwordgen code

---
+++ assets/javascript.js
@@ -0,0 +1,28 @@
+// ============ASSIGNING VARIABLES
+// snag the button
+// password length
+var passwordLength;
+// lowercase array
+// uppercase array
+// numbers array
+// spec chars array
+// piggybank array
+// text output field
+// output password
+
+// ============PROMPTS / STORING RESPONSES
+// listen to the button
+// how long is the passowrd?
+passwordLength = prompt("Choose a password length between 8 and 128 characters.")
+// lowercase? (if yes) store in empty piggybank array
+// uppercase? (if yes) store in empty piggybank array
+// spec characters? (if yes) store in empty piggybank array
+//  numbers? (if yes) store in empty piggybank array
+
+// ============GENERATING / RETURNED PASSWORD
+// for loop grab random characters from piggybank array - password length (tempArray)
+for(let i = 0; i < passwordLength; i++){
+
+}
+// join as string
+// print generated password onto page
